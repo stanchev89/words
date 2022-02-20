9fe2e2c48bf8e5c2813ee0250e101067ee4c70eb
Stefan Stanchev <stanchev89@gmail.com>
2022-02-20 13:59:36 +0200

    quick fix

--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@ export default {
         port: process.env.PORT || 3500,
         dbConnectionString: "mongodb://localhost:27017/cubes",
         staticPath: '/Users/stanchev/words/frontend/',
-        
     },
     production: {}
 };
